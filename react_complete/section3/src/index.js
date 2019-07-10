0fd2b2260812f53a86d8b4440d4dcafbf133e328
Danilo Barion Nogueira <danilo.barion@gmail.com>
2019-07-10 10:39:05 -0300

    Glimpse of reach hooks

--- react_complete/section3/src/index.js
+++ react_complete/section3/src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import FunctionalApp from './FunctionalApp';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<FunctionalApp />, document.getElementById('functional-root'));
+
 registerServiceWorker();
